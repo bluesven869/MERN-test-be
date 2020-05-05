d86b24015bd816582332633114e50b6f5378da92
Pedro Chen <bluesven869@gmail.com>
2020-05-05 21:45:41 +0800

    fix deprecated warning errors

--- lib/app.ts
+++ lib/app.ts
@@ -30,7 +30,8 @@ class App {
 
   private mongoSetup(): void {
     mongoose.Promise = global.Promise;
-    mongoose.connect(this.mongoUrl);
+    mongoose.connect(this.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, });
+    mongoose.set('useCreateIndex', true);
   }
 }
 
